87e24598b7e2bbfa20149460cd609996d2f75465
santana <delaramartinsmurilo@gmail.com>
2026-05-22 10:31:10 -0300

    Add files via upload

---
+++ react-lp-sesi04-main/src/App.jsx
@@ -0,0 +1,15 @@
+import s from './App.css'
+import img1 from '01/WhatsApp Image 2026-05-15 at 21.41.04.jpeg'
+
+function App() {
+//js
+
+  return (
+    <>
+      <h1>dsfdsfsd</h1>
+      <p>sadygdas</p>
+    </>
+  )
+}
+
+export default App
